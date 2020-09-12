80799b47b11c7ae93ba1e09f1a9a2d1475b947a8
Murat <murat51392@gmail.com>
2020-09-12 11:59:25 +0300

    picking-numbers

---
+++ easy/picking-numbers/picking-numbers.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pickingNumbers' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def pickingNumbers(a):
+    # Write your code here
+    lengths = []
+    for i in a:
+        currentList = []
+        for j in a:
+            if i-j == 0 or j-i == 1:
+                currentList.append(j)
+        lengths.append(len(currentList))
+    return max(lengths)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = pickingNumbers(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
